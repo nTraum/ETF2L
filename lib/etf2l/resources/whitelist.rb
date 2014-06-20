0f5fcc55391b663625abd95d6f1f5ab43ec634b3
nTraum <philipp.press@googlemail.com>
2014-06-20 16:27:09 +0200

    Add name attribute to Whitelist

--- lib/etf2l/resources/whitelist.rb
+++ lib/etf2l/resources/whitelist.rb
@@ -1,8 +1,9 @@
 module Etf2l
   class Whitelist
-    attr_reader :filename, :last_change, :url
+    attr_reader :name, :filename, :last_change, :url
 
     def initialize(attributes)
+      @name = attributes["name"]
       @filename = attributes["filename"]
       @last_change = Time.at(attributes["last_change"])
       @url = attributes["url"]
--- spec/lib/etf2l/resources/whitelist_spec.rb
+++ spec/lib/etf2l/resources/whitelist_spec.rb
@@ -1,9 +1,14 @@
 describe Whitelist do
+  let(:name) { "ETF2L Season 18" }
   let(:filename) { "etf2l_whitelist_6v6" }
   let(:last_change_unix_time) { 100 }
   let(:url) { "http://etf2l.org/configs/etf2l_whitelist_6v6.txt" }
-  let(:attributes) { { "filename" => filename, "last_change" => last_change_unix_time, "url" => url } }
+  let(:attributes) { { "name" => name, "filename" => filename, "last_change" => last_change_unix_time, "url" => url } }
   subject(:whitelist) { described_class.new(attributes)}
+
+  it "has a name" do
+    expect(whitelist.name).to eq(name)
+  end
 
   it "has a filename" do
     expect(whitelist.filename).to eq(filename)
